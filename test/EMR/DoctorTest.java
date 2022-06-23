d766be6b5f01517b2625f392f5052ad19f41733b
acshyguy <obeleagu.e@gmail.com>
2022-06-23 12:22:06 +0100

    Doctor class Test in Hospital Management

--- test/EMR/DoctorTest.java
+++ test/EMR/DoctorTest.java
@@ -1,10 +1,35 @@
 package EMR;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DoctorTest {
 
-    @org.junit.jupiter.api.BeforeEach
+    Doctor doctor;
+
+
+    @BeforeEach
     void setUp() {
+        doctor = new Doctor("Odogwu","Agu","Male","Pediatrician");
+
+    }
+
+    @Test
+    public void testForDocFirstName() {
+        assertEquals("Odogwu",doctor.getFirstName());
+    }
+    @Test
+    public void testForDocLastName() {
+        assertEquals("Agu",doctor.getLastName());
+    }
+    @Test
+    public void testForDocGender() {
+        assertEquals("Male",doctor.getGender());
+    }
+    @Test
+    public void testForDocSpecialty() {
+        assertEquals("Pediatrician",doctor.getSpecialty());
     }
 }

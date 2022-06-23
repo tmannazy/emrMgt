b12a9ad92224ff84604a4f542b52cb0164c1e3cf
acshyguy <obeleagu.e@gmail.com>
2022-06-23 12:16:22 +0100

    Doctor class in Hospital Management

--- src/EMR/Doctor.java
+++ src/EMR/Doctor.java
@@ -2,4 +2,33 @@ package EMR;
 
 public class Doctor {
 
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String doctorSpecialty;
+
+    public Doctor(String firstName, String lastName, String gender, String doctorSpecialty) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.doctorSpecialty = doctorSpecialty;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getSpecialty() {
+        return doctorSpecialty;
+    }
+
+
 }
